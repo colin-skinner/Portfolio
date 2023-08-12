40c41c6e1833486204e947114666438ce09363c4
colin-skinner <colinaskinner@gmail.com>
2023-08-12 09:48:19 -0400

    Changed some stuff

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
 
 window.onscroll = function() {myFunction()};
-var header = document.getElementById("my-header");
+var header = document.getElementById("navbar");
 
 
-var sticky = header.offsetTop;
-alert(typeof sticky)
+var sticky = 0; //header.offsetTop
+// alert(typeof sticky)
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        bottom: 0,
+        behavior: "auto",
+    });
+}
+
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function myFunction() {
-  if (window.scrollY > 0) {
+  if (window.scrollY > sticky) {
+    // alert(header)
     header.classList.add("sticky");
+    
   } else {
    
     header.classList.remove("sticky");
